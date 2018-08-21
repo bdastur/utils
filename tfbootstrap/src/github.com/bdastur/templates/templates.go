ec1f382c31b037e9f035652ecec418e126153f6a
Behzad Dastur <bdastur@gmail.com>
2018-08-20 17:21:37 -0700

    Add stderr output for the command execution.

--- tfbootstrap/src/github.com/bdastur/templates/templates.go
+++ tfbootstrap/src/github.com/bdastur/templates/templates.go
@@ -5,6 +5,7 @@ const (
 	providerTemplate = `
 provider "aws" {
 	region  = "{{ .Region }}"
+	profile = "{{ .Account }}"
 }
 `
 
@@ -14,11 +15,11 @@ terraform {
 	required_version = ">= v0.11.7"
 	backend "s3" {
 		region     = "{{ .Region }}"
-		profile    = "{{ .Profile }}"
-		bucket     = "{{ .Region }}-{{ .Profile }}-ilm-state"
-		key     = "/terraform-states/s3backends/{{ .Region }}-{{ .Profile }}-ilm-state.tfstate"
+		profile    = "{{ .Account }}"
+		bucket     = "{{ .Region }}-{{ .Account }}-ilm-state"
+		key     = "terraform-states/s3backends/{{ .Region }}-{{ .Account }}-ilm-state.tfstate"
 		encrypt    = "true"
-		dynamodb_table = "{{ .Region }}-{{ .Profile }}-dynamodb-table-tf-lock"
+		dynamodb_table = "{{ .Region }}-{{ .Account }}-dynamodb-table-tf-lock"
 		acl        = "bucket-owner-full-control"
 	}
 }
