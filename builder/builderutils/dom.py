3aa9a29d58d3639a30a7408400ec3f1f22989f82
Behzad Dastur <bdastur@gmail.com>
2019-05-30 12:20:13 -0700

    Pretty formatting

--- builder/builderutils/dom.py
+++ builder/builderutils/dom.py
@@ -21,11 +21,16 @@ class DomManager(object):
         if pretty:
             self.rendered_dom +=  "\n"
 
-    def parse_dom_tree(self, root):
+    def parse_dom_tree(self, root, indent=0):
         method_name = "render_{0}_element".format(root['element'])
+        if root['element'] == "html":
+            indent = 0
+        else:
+            indent += 2
+
         try:
             render_function = getattr(self, method_name)
-            rendered_string = render_function(root)
+            rendered_string = render_function(root, indent=indent)
             self.update_rendered_dom(rendered_string)
             print(rendered_string)
         except AttributeError:
@@ -35,63 +40,63 @@ class DomManager(object):
         if "children" in root:
             children = root['children'].keys()
             for child in children:
-                self.parse_dom_tree(root['children'][child])
-        end_tag = "</" + root["element"] + ">"
+                self.parse_dom_tree(root['children'][child], indent=indent)
+        end_tag = indent * " " + "</" + root["element"] + ">"
         print(end_tag)
         self.update_rendered_dom(end_tag)
 
     @staticmethod
-    def render_html_element(node):
-        tag = "<!DOCTYPE HTML>\n<html>"
+    def render_html_element(node, indent=0):
+        tag = indent * " " + "<!DOCTYPE HTML>\n<html>"
         return tag
 
     @staticmethod
-    def render_head_element(node):
-        tag = "<head>"
+    def render_head_element(node, indent=0):
+        tag = indent * " " + "<head>"
         return tag
 
     @staticmethod
-    def render_link_element(node):
-        renderedString = "<{0} rel={1} href=\"{2}\">".format(node["element"], node["rel"], node["href"])
+    def render_link_element(node, indent=0):
+        renderedString = indent * " " +  "<{0} rel={1} href=\"{2}\">".format(node["element"], node["rel"], node["href"])
         return renderedString
 
     @staticmethod
-    def render_div_element(node):
-        renderedString = "<{0} class={1}>".format(node["element"], node["class"])
+    def render_div_element(node, indent=0):
+        renderedString = indent * " " +  "<{0} class={1}>".format(node["element"], node["class"])
         return renderedString
 
     @staticmethod
-    def render_header_element(node):
-        renderedString = "<{0}>{1}".format(node["element"], node["text"])
+    def render_header_element(node, indent=0):
+        renderedString = indent * " " + "<{0}>{1}".format(node["element"], node["text"])
         return renderedString
 
     @staticmethod
-    def render_h1_element(node):
-        return DomManager.render_header_element(node)
+    def render_h1_element(node, indent=0):
+        return DomManager.render_header_element(node, indent=indent)
 
     @staticmethod
-    def render_h2_element(node):
-        return DomManager.render_header_element(node)
+    def render_h2_element(node, indent=0):
+        return DomManager.render_header_element(node, indent=indent)
 
     @staticmethod
-    def render_h3_element(node):
-        return DomManager.render_header_element(node)
+    def render_h3_element(node, indent=0):
+        return DomManager.render_header_element(node, indent=indent)
 
     @staticmethod
-    def render_h4_element(node):
-        return DomManager.render_header_element(node)
+    def render_h4_element(node, indent=0):
+        return DomManager.render_header_element(node, indent=indent)
 
     @staticmethod
-    def render_h5_element(node):
-        return DomManager.render_header_element(node)
+    def render_h5_element(node, indent=0):
+        return DomManager.render_header_element(node, indent=indent)
 
     @staticmethod
-    def render_h6_element(node):
-        return DomManager.render_header_element(node)
+    def render_h6_element(node, indent=0):
+        return DomManager.render_header_element(node, indent=indent)
 
     @staticmethod
-    def render_script_element(node):
-        rendered_string = "<{0} src=\"{1}\">".format(node["element"], node["src"])
+    def render_script_element(node, indent=0):
+        rendered_string = indent * " " + "<{0} src=\"{1}\">".format(node["element"], node["src"])
         return rendered_string
 
 
