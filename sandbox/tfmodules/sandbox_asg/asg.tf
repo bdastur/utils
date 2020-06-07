16fe38068bf661bfa4affe560bedcb3c4d672bc2
Behzad Dastur <bdastur@gmail.com>
2020-06-07 05:10:43 -0700

    Add module dependency between vpc and asg

--- sandbox/tfmodules/sandbox_asg/asg.tf
+++ sandbox/tfmodules/sandbox_asg/asg.tf
@@ -1,7 +1,12 @@
+variable "asg_depends_on" {
+    description = "Variable to force module to wait for VPC"
+}
+
 ###########################################################
 # Sandbox Launch template
 ###########################################################
 resource "aws_launch_template" "sandbox_lt" {
+  depends_on    = [var.asg_depends_on]
   name          = "sandbox_lt"
   instance_type = var.instance_type
   image_id      = var.ami_id
@@ -21,6 +26,42 @@ resource "aws_launch_template" "sandbox_lt" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+###########################################################
+# Autoscaling group
+###########################################################
+resource "aws_autoscaling_group" "sandbox_asg" {
+  depends_on = [
+    aws_launch_template.sandbox_lt
+  ]
+  name_prefix = "sandbox-asg"
+
+  vpc_zone_identifier       = [var.subnet_az1_id, var.subnet_az2_id]
+  min_size                  = 1
+  max_size                  = 1
+  desired_capacity          = 1
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  termination_policies      = ["OldestInstance", "OldestLaunchConfiguration"]
+  target_group_arns         = [""]
+  load_balancers            = [""]
+
+  launch_template {
+    id      = aws_launch_template.sandbox_lt.id
+    version = "$Latest"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag {
+    key                 = "Sandbox"
+    value               = var.sandbox_name
+    propagate_at_launch = true
+  }
 
 }
+
 
