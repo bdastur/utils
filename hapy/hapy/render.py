3422a2874c64d9fb8dc6a94f6a1ae64133bd3026
Behzad Dastur <bdastur@gmail.com>
2021-01-19 19:09:46 -0800

    Add function to render all j2 templates and stage them

--- hapy/hapy/render.py
+++ hapy/hapy/render.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import jinja2
+from  loguru import logger
+import shutil
 
 '''
 Jinja2 Render Util
@@ -12,7 +15,7 @@ class Render(object):
     def __init__(self):
         pass
 
-    def render_j2_template_file(self, templateFile, searchPath, renderObj):
+    def render_j2_template_file(self, templateFile, searchPath, **kwargs):
         """ The API will render a Jinja2 template
 
         :type  templateFile:  String
@@ -24,17 +27,56 @@ class Render(object):
         :type  renderObj:  dict
         :param renderObj: Dictionary object to substitute the template vars
 
-        :returns:
+        :returns: Rendered string
         """
         template_loader = jinja2.FileSystemLoader(searchpath=searchPath)
         env = jinja2.Environment(
             loader=template_loader, trim_blocks=True, lstrip_blocks=True
         )
         template = env.get_template(templateFile)
-        renderedData = template.render(renderObj)
+        renderedData = template.render(kwargs)
 
         return renderedData
 
+    def render_j2_template_dir(self, templateDir, destDir, ext=None, **kwargs):
+        """
+        The API will render all <file>.j2 files to corresponding <file>.<ext>
+
+        :param templateDir Path to where the templates are
+        :param destDir     Path to save the rendered files.
+        :param ext         Extension to add to the rendered files
+        :param **kwargs   Options
+
+        Options:
+        dest_path  The destination path where to copy the rendered files (Default: templateDir)
+        ext        The file extension to apply to the rendered files.
+        """
+        if not os.path.exists(templateDir):
+            logger.error("templateDir {} does not exist", templateDir)
+            return -1
+
+        # Create destDir if does not exist.
+        if not os.path.exists(destDir):
+            os.mkdir(destDir)
+
+        template_files = os.listdir(templateDir)
+        for filename in template_files:
+            src_file_path = os.path.join(templateDir, filename)
+
+            filename = os.path.splitext(filename)[0]
+            if ext is not None:
+                filename += ".%s" % ext
+            dest_file_path = os.path.join(destDir, filename)
+
+            rendered_data = self.render_j2_template_file(src_file_path,
+                                                         ".",
+                                                         **kwargs)
+            print("Renndered data: ", rendered_data)
+            with open(dest_file_path, 'w') as fhandle:
+                fhandle.write(rendered_data)
+
+        return 0
+        
     def render_j2_template_string(self, templateString, **kwargs):
         """ one line description
 
